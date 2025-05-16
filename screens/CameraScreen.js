072633b4d38e46e87006f3350cdaff32f684d7b6
Falllstreak <23.anomaly@gmail.com>
2025-05-15 19:30:09 -0700

    Added comment line 33

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
 import { Camera } from 'expo-camera';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -30,7 +30,7 @@ export default function CameraScreen() {
     </View>
   );
 }
-
+// Testing style
 const styles = StyleSheet.create({
   container: { flex: 1 },
   camera: { flex: 1 },
